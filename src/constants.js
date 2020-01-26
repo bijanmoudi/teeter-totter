9c7cd09094bdfc3160cd054c872958f3a572ba69
Bijan Moudi <www.moudi@gmail.com>
2020-01-26 05:32:43 +0330

    Updates

--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export * from './components/Seesaw/constants';
--- src/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import Seesaw from './components/Seesaw/reducer';
-
-const reducer = combineReducers({
-	Seesaw
-});
-
-export default reducer;
